f6ed4b171676bc2f350f917eabf1392bd13bbe75
Brendan Chen <me@bchen.dev>
2024-04-04 16:51:02 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Brendan Chen on 2024.04.04.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
